ade4d370a9418836e80b61fba48d33ea35851279
Shibi K R <shibik@thoughtworks.com>
2022-05-27 22:54:12 +0530

    cleanup

--- src/main/java/water_bill/WaterBillApplication.java
+++ src/main/java/water_bill/WaterBillApplication.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 
 public class WaterBillApplication {
     public static void main(String[] args) throws IOException {
-        System.out.println("Welcome to water bill generation system!");
         if(args.length != 0 && args[0] != null) {
             FileProcessor fileProcessor = new FileProcessor(args[0]);
             WaterBillInput waterBillInput = fileProcessor.readFile();
